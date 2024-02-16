249c491c77898d69baca89a6ec6ac62c4f976495
Alexander-Anischenko <156853009+Alexander-Anischenko@users.noreply.github.com>
2024-02-16 22:04:11 +0100

    Cat

---
+++ Learning_AIT_42.1/src/lesson15/Lesson15.java
@@ -0,0 +1,20 @@
+package lesson15;
+
+/**
+ * AIT-TR, cohort 42.1, Java Basic, Lesson #15
+ *
+ * @version 16. 02. 24
+ */
+public class Lesson15 {
+    public static void main(String[] args) {
+        Cat cat = new Cat();//"Mursik", "white", 3);
+        System.out.println("Cat: " + cat.name + ", " + cat.color + ", "  + cat.age);
+        cat.voice();
+
+        Cat cat1 = new Cat("Barsik", "black", 5 , 10);
+        System.out.println( "Cat 1: " + cat1.name + ", " + cat1.color + ", "  + cat1.age);
+        cat1.voice();
+        cat1.eat(5);
+        System.out.println("Satiety level: " + cat1.satiated());
+    }
+}
