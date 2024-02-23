aebafd904a60b4525bd727b0f66b79f08391a166
Alexander-Anischenko <156853009+Alexander-Anischenko@users.noreply.github.com>
2024-02-23 14:27:33 +0100

    Update Plate.java

--- Learning_AIT_42.1/src/lesson16/Plate.java
+++ Learning_AIT_42.1/src/lesson16/Plate.java
@@ -14,7 +14,7 @@ public class Plate {
     public void add(int food){
         if ( this.food + food > capacity){
             this.food = capacity;
-            return;
+            return; 
         }
         this.food += food;
     }
