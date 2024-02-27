6eb84e27a1e00b7efd8ef3ff0dac12cc50b084cd
Alexander-Anischenko <156853009+Alexander-Anischenko@users.noreply.github.com>
2024-02-27 12:38:19 +0100

    Update HomeWork16

--- Learning_AIT_42.1/src/lesson16/homework/HomeWork16.java
+++ Learning_AIT_42.1/src/lesson16/homework/HomeWork16.java
@@ -7,10 +7,12 @@ import lesson16.Plate;
  * AIT-TR, cohort 42.1, Java Basic, Home work   #16
  *
  * @autor Alexander Anischenko
- * @version 23-02-24
+ * @version 27-02-24
  */
 public class HomeWork16 {
     public static void main(String[] args) {
+
+        // Task #1
         Cat[] cats = {new Cat(12), new Cat(16), new Cat(10), new Cat(9)};
         Plate plate = new Plate(60);
         System.out.println("Cats are hungry!");
@@ -21,5 +23,14 @@ public class HomeWork16 {
             cat.eat(plate);
             System.out.println("Cat: " + ((double)cat.realVolume/cat.maxVolume) * 100 +"%");
         }
+
+        // Task #2
+        Car car = new Car(64);
+        PetrolStation station = new PetrolStation(1000);
+        System.out.println("Car: " + car.fuel);
+        System.out.println("Station: " + station.fuel);
+        car.refuel(station);
+        System.out.println("Car: " + car.fuel);
+        System.out.println("Station: " + station.fuel);
     }
 }
---
+++ Learning_AIT_42.1/src/lesson16/homework/PetrolStation.java
@@ -0,0 +1,20 @@
+package lesson16.homework;
+
+public class PetrolStation {
+
+    double fuel;
+
+
+
+    public PetrolStation(double fuel) {
+        this.fuel = fuel;
+    }
+
+    public double decreaseFuel(double fuel) {
+        if (this.fuel - fuel <0) {
+            return 0;
+        }
+        this.fuel -= fuel;
+        return fuel;
+    }
+}
